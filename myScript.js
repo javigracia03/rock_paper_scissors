1d04fc88982d601efa08ff208176ea777542dab8
Javier Gracia <javiergracia2003@gmail.com>
2022-11-10 13:25:44 +0100

    One round of the game added in the console

--- myScript.js
+++ myScript.js
@@ -4,7 +4,6 @@ let comp_choice = () => {
 
     const ran_num = Math.floor(Math.random() * 3);
     let option;
-    console.log(ran_num);
 
     if(ran_num === 0){
 
@@ -17,9 +16,56 @@ let comp_choice = () => {
         option = "scissors";
 
     }
+    console.log(option + "computer");
 
-    console.log(option);
+    return option;
 
 };
 
-comp_choice();
+let user_choice = prompt("TYPE ROCK, PAPER OR SCISSORS").toLowerCase();
+console.log(user_choice);
+let winner;
+let congrats;
+
+function playRound(comp_choice, user_choice){
+
+    
+    if (comp_choice === user_choice){
+        winner = null;
+    }
+    else if (comp_choice === "rock" & user_choice=== "paper"){
+        winner = "user";
+
+    }
+    else if (comp_choice === "rock" & user_choice=== "scissors"){
+        winner = "computer";
+
+    }
+    else if (comp_choice === "paper" & user_choice=== "rock"){
+        winner = "computer";
+
+    }
+    else if (comp_choice === "paper" & user_choice=== "scissors"){
+        winner = "user";
+
+    }
+    else if (comp_choice === "scissors" & user_choice=== "paper"){
+        winner = "computer";
+
+    }
+    else if (comp_choice === "scissors" & user_choice=== "rock"){
+        winner = "user";
+
+    }
+
+    if (winner==="user"){
+
+        congrats = `You won! ${user_choice} beats ${comp_choice}`
+    }
+    else{
+        congrats = `You lose! ${comp_choice} beats ${user_choice}`
+    }
+
+    return congrats;
+}
+console.log(playRound(comp_choice(), user_choice));
