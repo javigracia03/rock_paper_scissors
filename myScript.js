c70db39efbee356b34eeebfbd82e77ad259d236e
Javier Gracia <javiergracia2003@gmail.com>
2022-11-10 10:18:52 +0100

    added choice between rock, paper and scissors

--- myScript.js
+++ myScript.js
@@ -1,1 +1,25 @@
 console.log("Hello World");
+
+let comp_choice = () => { 
+
+    const ran_num = Math.floor(Math.random() * 3);
+    let option;
+    console.log(ran_num);
+
+    if(ran_num === 0){
+
+        option = "rock";
+    }
+    else if(ran_num === 1){
+        option = "paper";
+    }
+    else{
+        option = "scissors";
+
+    }
+
+    console.log(option);
+
+};
+
+comp_choice();
