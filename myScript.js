34a66b75357c621724437dd514d9b7443ee181b6
Javier Gracia <javiergracia2003@gmail.com>
2022-11-11 10:01:10 +0100

    Counter  added

--- myScript.js
+++ myScript.js
@@ -1,20 +1,22 @@
 console.log("Hello World");
 
 
+    let us_count = 0;
+    let cpu_count = 0;
+
    window.onload = function(){
 
     const rock_btn = document.getElementById('el_rock');
     const paper_btn = document.getElementById('el_paper');
     const scissors_btn = document.getElementById('el_scissors');
-    const counter = document.getElementById('counter');
     
-    console.log(rock_btn);
+    
+    console.log(counter);
     
     rock_btn.addEventListener("click", () => game("rock"));
     paper_btn.addEventListener("click", () => game("paper"));
     scissors_btn.addEventListener("click", () => game("scissors"));
     
-    return counter;
     
    } 
 
@@ -42,12 +44,11 @@ let comp_choice = () => {
 
 };
 
-let winner;
-let congrats;
 
 function playRound(comp_choice, user_choice){
 
-
+    let winner;
+    let congrats;
     
     if (comp_choice === user_choice){
         winner = null;
@@ -83,13 +84,17 @@ function playRound(comp_choice, user_choice){
 }
 
 function congrat (winner, user_choice, comp_choice){
+
+
+    
     let congrats;
     if (winner==="user"){
-
+        us_count ++;
         congrats = `You won! ${user_choice} beats ${comp_choice}`
     }
     else if (winner==="computer"){
         congrats = `You lose! ${comp_choice} beats ${user_choice}`
+        cpu_count++;
     }
     else congrats = "There was a draw";
 
@@ -97,26 +102,37 @@ function congrat (winner, user_choice, comp_choice){
 }
 /*console.log(playRound(comp_choice(), user_choice));*/
 
-function game(user_choice, counter){
+function game(user_choice){
 
-    for (let i=0; i<5;i++){
-        /*let user_choice = prompt("TYPE ROCK, PAPER OR SCISSORS").toLowerCase();*/
+        const cpu_score = document.getElementById("comp_score");
+        const user_score = document.getElementById("user_score");
+        const counter = document.getElementById('counter');
+
+
+            /*let user_choice = prompt("TYPE ROCK, PAPER OR SCISSORS").toLowerCase();*/
+            
+            let compu_choice;
+            compu_choice = comp_choice();
+            
+            
+            console.log(counter);
+            let winner_round = playRound(compu_choice, user_choice);
+            let congrats =congrat(winner_round, user_choice, compu_choice);
+            console.log(congrats);
+             /*alert(congrat(winner_round, user_choice, compu_choice));*/
+            counter.innerHTML = congrats;
+            user_score.innerHTML = us_count;
+            cpu_score.innerHTML = cpu_count;
+            
         
-        let compu_choice;
-        compu_choice = comp_choice();
-        
-        
-        console.log(counter);
-        let winner_round = playRound(compu_choice, user_choice);
-        console.log(congrat(winner, user_choice, compu_choice));
-        alert(congrat(winner, user_choice, compu_choice));
-        counter.innerHTML = "HELLO";
-    }
+    
+
+    
 
     
 
 }
-game();
+
 
 function getUserChoice(){
     let user_choice;
